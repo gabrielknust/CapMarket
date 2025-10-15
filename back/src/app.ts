79c061bc7772d0a50da80a3aa783126d570f4c77
Gabriel Knust <gabrielsilvaknustws@gmail.com>
2025-10-15 17:51:53 -0300

    fix: fixed pino logger

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,4 +1,6 @@
-import express from "express";
+// src/app.ts
+
+import express, { Request, Response, NextFunction } from "express";
 import pino from "pino";
 import pretty from "pino-pretty";
 import pinoHttp = require("pino-http");
@@ -6,6 +8,16 @@ import userRoutes from "./routes/user.routes";
 import productRoutes from "./routes/product.routes";
 
 const app = express();
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const originalJson = res.json;
+  res.json = function (body) {
+    res.locals.responseBody = body;
+    return originalJson.call(this, body);
+  };
+  next();
+});
+
 const stream = pretty({
   colorize: true,
   translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
@@ -17,6 +29,11 @@ const pinoLogger = pino(stream);
 
 const loggerMiddleware = (pinoHttp as any)({
   logger: pinoLogger,
+  customProps: function (req: Request, res: Response) {
+    return {
+      responseBody: res.locals.responseBody,
+    };
+  },
 });
 
 app.use(loggerMiddleware);
