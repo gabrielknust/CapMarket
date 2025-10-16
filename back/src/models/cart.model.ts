84aa4a4d0d077fa3a7807ac5e96584b8efb594a1
Gabriel Knust <gabrielsilvaknustws@gmail.com>
2025-10-16 13:07:29 -0300

    feat: add cart model

---
+++ back/src/models/cart.model.ts
@@ -0,0 +1,47 @@
+import { Schema, model, Document, PopulatedDoc } from "mongoose";
+import { IProduct } from "./product.model";
+import { IUser } from "./user.model";
+
+interface ICartItem {
+  product: PopulatedDoc<IProduct & Document>;
+  quantity: number;
+}
+
+export interface ICart extends Document {
+  customer: PopulatedDoc<IUser & Document>;
+  itens: ICartItem[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const cartSchema = new Schema<ICart>(
+  {
+    customer: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    itens: [
+      {
+        product: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+          default: 1,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Cart = model<ICart>("Cart", cartSchema);
+export default Cart;
