a74f4223207640775c561df51b2f6244b5c42e9a
Gabriel Knust <gabrielsilvaknustws@gmail.com>
2025-10-17 19:48:40 -0300

    feat: remove pino and add morgan

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,10 +1,10 @@
 import express, { Request, Response, NextFunction } from "express";
-import pino from "pino";
-import pretty from "pino-pretty";
-import pinoHttp = require("pino-http");
 import userRoutes from "./routes/user.routes";
 import productRoutes from "./routes/product.routes";
 import loginRoutes from "./routes/login.routes";
+import cartRoutes from "./routes/cart.routes";
+import orderRoutes from "./routes/order.routes";
+import morgan from "morgan";
 
 const app = express();
 
@@ -17,30 +17,14 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   next();
 });
 
-const stream = pretty({
-  colorize: true,
-  translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
-  ignore: "pid,hostname",
-  sync: true,
-});
-
-const pinoLogger = pino(stream);
-
-const loggerMiddleware = (pinoHttp as any)({
-  logger: pinoLogger,
-  customProps: function (req: Request, res: Response) {
-    return {
-      responseBody: res.locals.responseBody,
-    };
-  },
-});
-
-app.use(loggerMiddleware);
+app.use(morgan("dev"));
 
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/login", loginRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/orders", orderRoutes);
 
 export default app;
